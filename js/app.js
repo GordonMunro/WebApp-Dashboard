f34b8e68c5b7a58f57303ff567be08ab7b8fbf95
GordonMunro <82704461+GordonMunro@users.noreply.github.com>
2021-09-07 16:05:35 -0700

    updates

---
+++ js/app.js
@@ -0,0 +1,58 @@
+// Alert Banner
+const alertBanner = document.getElementById("alert");
+alertBanner.innerHTML =
+`
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+</div>
+`;
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = 'none';
+    }
+});
+// Chart Widgets
+// Traffic Chart
+const trafficCanvas = document.getElementById('traffic-chart');
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: ['rgba(116, 119, 191, 0.3)', 'blue', 'green', 'yellow', 'red', 'grey', 'rgba(119, 126, 191, 0.3)', 'tomato', 'lightslategrey', 'skyblue', 'lightgreen'],
+        borderWidth: 1,
+    }]
+};
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+          beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+          display: false
+        }
+    }
+};
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+// Bar Graph
+const dailyCanvas = document.getElementById("daily-chart");
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
